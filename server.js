78d7326a2e17cee7d7c1d797da62168b27bab7db
snd112 <snd555890@gmail.com>
2026-05-09 04:01:54 +0300

    Change server port from 3000 to 8080

--- server.js
+++ server.js
@@ -47,6 +47,6 @@ message:endpoint+' endpoint ready'
 });
 });
 
-app.listen(3000,()=>{
-console.log('Server running on port 3000');
+app.listen(8080,()=>{
+console.log('Server running on port 8080');
 });
